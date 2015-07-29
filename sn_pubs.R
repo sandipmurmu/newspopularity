a630191c20bd6460e227c9efadae8fcdeec7e1f4
Mursan <sandip.murmu@gmail.com>
2015-07-30 04:52:44 +0530

    update the script

--- sn_pubs.R
+++ sn_pubs.R
@@ -4,36 +4,115 @@ library(flexclust)
 library(randomForest)
 library(miscTools)
 
+ds = read.csv(file.choose(), header=TRUE)
 
-mashpubs=read.csv(file.choose(),header = TRUE)
-pubs = mashpubs
+##Data partition into 2 subsets - 80% train and 20% test
+indexes = sample(1:nrow(ds), size = 0.2*nrow(ds))
+test = ds[indexes,]
+dim(test)
+train = ds[-indexes,]
+dim(train)
 
-pubs$url=NULL
-pubs$timedelta=NULL
-pubs$data_channel_is_bus=NULL
-pubs$data_channel_is_tech=NULL
-pubs$data_channel_is_world=NULL
-pubs$data_channel_is_socmed=NULL
-pubs$data_channel_is_lifestyle=NULL
-pubs$data_channel_is_entertainment=NULL
-pubs$shares=NULL
 
-preproc = preProcess(pubs)
-normdata = predict(preproc,pubs)
+##Create Clusters using K-means
+strain=train
 
-km = kmeans(normdata,centers = 3)
-km.kcca = as.kcca(km,normdata)
-clusterdata = predict(km.kcca)
+strain$url=NULL
+strain$timedelta=NULL
+strain$data_channel_is_bus=NULL
+strain$data_channel_is_tech=NULL
+strain$data_channel_is_world=NULL
+strain$data_channel_is_socmed=NULL
+strain$data_channel_is_lifestyle=NULL
+strain$data_channel_is_entertainment=NULL
 
-table(clusterdata)
+strain$is_weekend=NULL
+strain$weekday_is_monday=NULL
+strain$weekday_is_tuesday=NULL
+strain$weekday_is_wednesday=NULL
+strain$weekday_is_thursday=NULL
+strain$weekday_is_friday=NULL
+strain$weekday_is_saturday=NULL
+strain$weekday_is_sunday=NULL
+  
 
-cluster1 = subset(mashpubs,clusterdata==1)
-cluster2 = subset(mashpubs,clusterdata==2)
-cluster3 = subset(mashpubs,clusterdata==3)
+stest = test
 
-modelRef1 = randomForest(log(shares) ~ ., data = cluster1, ntree=10000, importance=TRUE, keep.forest=TRUE, do.trace=1000)
-modelRef2 = randomForest(log(shares) ~ ., data = cluster2, ntree=10000, importance=TRUE, keep.forest=TRUE, do.trace=1000)
-modelRef3 = randomForest(log(shares) ~ ., data = cluster3, ntree=10000, importance=TRUE, keep.forest=TRUE, do.trace=1000)
+stest$url=NULL
+stest$timedelta=NULL
+stest$data_channel_is_bus=NULL
+stest$data_channel_is_tech=NULL
+stest$data_channel_is_world=NULL
+stest$data_channel_is_socmed=NULL
+stest$data_channel_is_lifestyle=NULL
+stest$data_channel_is_entertainment=NULL
+
+stest$is_weekend=NULL
+stest$weekday_is_monday=NULL
+stest$weekday_is_tuesday=NULL
+stest$weekday_is_wednesday=NULL
+stest$weekday_is_thursday=NULL
+stest$weekday_is_friday=NULL
+stest$weekday_is_saturday=NULL
+stest$weekday_is_sunday=NULL
+
+## use caret functions to preprocess the data
+prepProcess = preProcess(strain)
+ntrain = predict(prepProcess,strain)
+ntest = predict(prepProcess,stest)
+
+##Create 3 clusers
+km = kmeans(ntrain,centers = 3)
+
+##Create classification for train and test
+km.kcca = as.kcca(km,ntrain)
+ctrain = predict(km.kcca)
+ctest = predict(km.kcca, newdata=ntest)
+
+table(ctrain)
+table(ctest)
+
+## Subset the train and test sets
+train1 = subset(train, ctrain == 1)
+train2 = subset(train, ctrain == 2)
+train3 = subset(train, ctrain == 3)
+
+test1 = subset(test, ctest == 1)
+test2 = subset(test, ctest == 2)
+test3 = subset(test, ctest == 3)
+
+modelRef1 = randomForest(log(shares) ~ . - url, data = train1, ntree=10000, importance=TRUE, keep.forest=TRUE, do.trace=1000)
+modelRef2 = randomForest(log(shares) ~ . - url, data = train2, ntree=10000, importance=TRUE, keep.forest=TRUE, do.trace=1000)
+modelRef3 = randomForest(log(shares) ~ . - url, data = train3, ntree=10000, importance=TRUE, keep.forest=TRUE, do.trace=1000)
+
+## Test the prediction on train set
+pred_train1 = predict(modelRef1, train1)
+pred_train2 = predict(modelRef2, train2)
+pred_train3 = predict(modelRef3, train3)
+
+preds_train = c(exp(pred_train1), exp(pred_train2), exp(pred_train3))
+vals_train = c(train1$shares, train2$shares, train3$shares)
+
+## Generate rsquared and rmse
+rSquared(vals_train,vals_train - preds_train)
+sqrt(mean((vals_train-preds_train)^2))
+
+
+## Generate predictions on test set
+pred_test1 = predict(modelRef1, test1)
+pred_test2 = predict(modelRef2, test2)
+pred_test3 = predict(modelRef3, test3)
+
+
+## Combine results from individual sets after exponentiating the predictions
+pred_test = c(exp(pred_test1), exp(pred_test2), exp(pred_test3))
+allIds = c(test1$url, test2$url, test3$url)
+
+## Sort as per id and generate the file for submission
+out_df = data.frame(allIds, preds_test)
+out_df = out_df[with(out_df, order(allIds, preds_test)), ]
+names(out_df) = c("id", "predictions")
+write.csv(out_df, "KM_RF_R_AKumar.csv", row.names=F, quote=F)
 
 
 
